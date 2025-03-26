651f93987ee4bfb5c9377c33bcfcb08d1002f4f8
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-03-26 12:56:48 +0900

    [FEAT] CustomerManager

--- Assets/Scripts/JH/CustomerManager.cs
+++ Assets/Scripts/JH/CustomerManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CustomerManager
 {
-    int LeftCustomers { get { return _leftCustomers; } set { _leftCustomers = value; } }
-    int _leftCustomers;
+    public int LeftCustomers { get { return _leftCustomers; } set { _leftCustomers = value; } }
+    private int _leftCustomers;
     
 
     public void Init()
@@ -18,9 +18,16 @@ public class CustomerManager
         LeftCustomers = 5;
     }
 
+    private void EndDay()
+    {
+        // 손님 그만 받고 일일 정산.
+    }
+
     private void CreateCustomer()
     {
+        //if (LeftCustomers > 0) {
 
+        //OrderDatabase.ObjectData.TryGetValue()
     }
     // 인풋: 첫 날인지 여부
     // 해야햐는 것: 손님의 수 만큼 랜덤한 손님 데이터 뽑아서 미니게임 시작
