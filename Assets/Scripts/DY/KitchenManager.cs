429dc2fcac47a9ca3bf6cff7a625b1d7c53ae204
Dongyeong Kim <dyeongkim98@gmail.com>
2025-03-26 22:08:41 +0900

    [FEAT] 로스율 곰팡이 비율 추가

--- Assets/Scripts/DY/KitchenManager.cs
+++ Assets/Scripts/DY/KitchenManager.cs
@@ -5,19 +5,19 @@ public class KitchenManager
 {
     private int _orderKey;
     public int OrderKey { get{return _orderKey;} set{_orderKey = value;} }
+
+    // 게임 결과
+    //남은 음식 영역
+    private float _resultRemainingPercentage;
+    //곰팡이 덮인 비율
+    private float moldPercentage;
     
-    public Action<int> order;
+    public float ResultRemainingPercentage { get{return _resultRemainingPercentage;} set{_resultRemainingPercentage = value; }}
+    public float MoldPercentage { get{return moldPercentage;} set{moldPercentage = value; }}
     
     public void Init()
     {
         
     }
-    
-    // 주문 받기
-    public void Order(int orderNum)
-    {
-        order?.Invoke(orderNum);
-    }
-    
      
 }
