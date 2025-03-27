96e1af5846511837e73246513c57eed4119ccc22
pdefd77 <pdefd77@gmail.com>
2025-03-27 11:16:08 +0900

    [Fix] 엔딩씬에 스크립트가 없던 문제 수정

--- Assets/Scripts/DH/PlayEnding.cs
+++ Assets/Scripts/DH/PlayEnding.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 public class PlayEnding : MonoBehaviour
 {
     private Canvas _canvas;
-    private GameObject[] _openingObjects;
+    private GameObject[] _endingObjects;
     private TypingText _typingText;
     private WaitForSeconds _delay;
 
     private void Awake()
     {
         _canvas = FindAnyObjectByType<Canvas>();
-        _openingObjects = new GameObject[_canvas.transform.childCount];
+        _endingObjects = new GameObject[_canvas.transform.childCount];
         for (int i = 0; i < _canvas.transform.childCount; i++)
         {
-            _openingObjects[i] = _canvas.transform.GetChild(i).gameObject;
+            _endingObjects[i] = _canvas.transform.GetChild(i).gameObject;
         }
         _delay = new WaitForSeconds(50 * Time.deltaTime);
     }
 
     private void Start()
     {
-        foreach (GameObject openingObject in _openingObjects) openingObject.SetActive(false);
+        foreach (GameObject openingObject in _endingObjects) openingObject.SetActive(false);
 
         StartCoroutine(StartEnding());
     }
@@ -31,37 +31,37 @@ public class PlayEnding : MonoBehaviour
         switch (Manager.Game.EndingType)
         {
             case 1: // 해피엔딩
-                _openingObjects[0].SetActive(true);
+                _endingObjects[0].SetActive(true);
 
                 yield return _delay;
 
-                _openingObjects[3].SetActive(true);
+                _endingObjects[3].SetActive(true);
 
-                _typingText = _openingObjects[3].GetComponent<TypingText>();
+                _typingText = _endingObjects[3].GetComponent<TypingText>();
 
                 while (!_typingText.IsDone) yield return null;
 
                 break;
             case 2: // 파산엔딩
-                _openingObjects[1].SetActive(true);
+                _endingObjects[1].SetActive(true);
 
                 yield return _delay;
 
-                _openingObjects[4].SetActive(true);
+                _endingObjects[4].SetActive(true);
 
-                _typingText = _openingObjects[3].GetComponent<TypingText>();
+                _typingText = _endingObjects[3].GetComponent<TypingText>();
 
                 while (!_typingText.IsDone) yield return null;
 
                 break;
             case 3: // 식중독엔딩
-                _openingObjects[2].SetActive(true);
+                _endingObjects[2].SetActive(true);
 
                 yield return _delay;
 
-                _openingObjects[3].SetActive(true);
+                _endingObjects[3].SetActive(true);
 
-                _typingText = _openingObjects[5].GetComponent<TypingText>();
+                _typingText = _endingObjects[5].GetComponent<TypingText>();
 
                 while (!_typingText.IsDone) yield return null;
 
