6a6bc83f5809cca05e8312df1b84aaef49a1018f
pdefd77 <pdefd77@gmail.com>
2025-03-29 20:10:49 +0900

    [Fix] 재료 조각 낙하 속도 수정
    DeltaTime을 식에 포함

--- Assets/Scripts/DH/CuttedIngridient.cs
+++ Assets/Scripts/DH/CuttedIngridient.cs
@@ -3,13 +3,6 @@ using UnityEngine;
 
 public class CuttedIngridient : MonoBehaviour
 {
-    private MoldSpawner _moldSpawner;
-
-    private void Awake()
-    {
-        _moldSpawner = GetComponentInParent<MoldSpawner>();
-    }
-
     public void Start()
     {
         StartCoroutine(Throwing());
@@ -21,13 +14,13 @@ public class CuttedIngridient : MonoBehaviour
         Vector2 nowPosition = transform.position;
         float t = 0f;
 
-        for (int i = 0; i < 1000; i++)
+        for (int i = 0; i < 3000; i++)
         {
+            transform.position = new(nowPosition.x + velocity.x * t, nowPosition.y + velocity.y * t - 10f * t * t / 2); // 잘린 재료는 포물선으로 이동 후 제거
+
             //if (_moldSpawner.MoldSet.Count == 0) t += 0.015f; // 끝난다면 빠르게 내려가기
             //else t += 0.003f;
-            t += 0.003f;
-
-            transform.position = new(nowPosition.x + velocity.x * t, nowPosition.y + velocity.y * t - 10f * t * t / 2); // 잘린 재료는 포물선으로 이동 후 제거
+            t += 2 * Time.deltaTime;
 
             yield return null;
         }
